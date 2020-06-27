7bac293f75946e1dfca50e8b906b9fc57b7e774a
Luã Braga <ldemourabraga@gmail.com>
2020-06-26 23:13:40 -0300

    Get list Api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
+import api from './services/api';
+
+//Não possuem valor semântico (significado)
+//Não possuem estilização própria
+//Todos componentes possuem por padrão 'display: flex'
+
+//View: div, footer, header, main, aside, section
+//Text: p, span, strong, h1, h2, h3
+
 export default function App() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
     return (
         <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
             <View style={styles.container}>
-                <Text style={styles.title}>Hello GoStack!</Text>
+                {projects.map(project => (
+                    <Text style={styles.project} key={project.id}>{project.title}</Text>
+                ))}
             </View>
         </>
     );
@@ -19,9 +39,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    title: {
+    project: {
         color: '#fff',
-        fontSize: 32,
-        fontWeight: 'bold'
+        fontSize: 20,
     }
 });
