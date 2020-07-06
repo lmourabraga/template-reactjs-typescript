1189a8fc990b1d5cf1c18986475e93a5bd98e2da
Luã Braga <ldemourabraga@gmail.com>
2020-07-05 23:12:23 -0300

    Creating model

--- first-project-node/src/routes/appointments.routes.ts
+++ first-project-node/src/routes/appointments.routes.ts
@@ -1,18 +1,26 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns'; // Lib para formatar datas e horas
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
-    const appointment = {
-        id: uuid(), // instalar a lib uuidv4 para gerar os ids.
-        provider,
-        date,
-    };
+    const parsedDate = startOfHour(parseISO(date));
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parsedDate, appointment.date),
+    );
+
+    if (findAppointmentInSameDate) {
+        return response
+            .status(400)
+            .json({ message: 'This appointment is already booked.' });
+    }
+
+    const appointment = new Appointment(provider, parsedDate);
 
     appointments.push(appointment);
 
