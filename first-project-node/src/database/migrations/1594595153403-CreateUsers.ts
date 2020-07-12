b239acda086387d757b89b3398ef3253dbbb4e27
Luã Braga <ldemourabraga@gmail.com>
2020-07-12 20:35:18 -0300

    Creating user table

--- first-project-node/src/database/migrations/1594595153403-CreateUsers.ts
+++ first-project-node/src/database/migrations/1594595153403-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1594493220983
-    implements MigrationInterface {
+export default class CreateUsers1594595153403 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -15,12 +14,17 @@ export default class CreateAppointments1594493220983
                         default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone', // O type abaixo só existe no postgres. Se usar outro banco usar apenas timestamp
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'created_at',
@@ -38,6 +42,6 @@ export default class CreateAppointments1594493220983
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 }
