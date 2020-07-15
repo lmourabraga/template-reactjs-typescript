665abea693e0a5e7a0553551963267bf02498aec
Luã Braga <ldemourabraga@gmail.com>
2020-07-15 20:14:53 -0300

    Creating Authentication

---
+++ first-project-node/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'cc4a615f75e5de8b257ebed35d7f162f',
+        expiresIn: '1d',
+    },
+};
--- first-project-node/src/routes/appointments.routes.ts
+++ first-project-node/src/routes/appointments.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns'; // Lib para formatar datas e horas
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
